56ea942f8cd3b9ae20204b7268f804c41b1f7e42
Jordan <soundlogic@wavesandbox.com>
2015-08-16 13:31:17 -0400

    Update Program.cs

--- Diagrams/Program.cs
+++ Diagrams/Program.cs
@@ -12,7 +12,14 @@ using Microsoft.CodeAnalysis.FindSymbols;
 namespace DotNetDiagrams
 {
     /// <summary>
-    /// this is a proof of concept and the project will be more formalized soon
+    /// this is a proof of concept brute-force approach to get familiar with Roslyn
+    /// - the Wiki provides details on usage: https://github.com/SoundLogic/Diagrams/wiki
+    /// 
+    /// For interested parties:
+    /// Due to some of the limitations of Roslyn (the nature of analyzing IL - lambdas/iterator 
+    /// methods/await | accessing code in external DLLs ), I did not continue
+    /// to pursue / formalize this project - it would likely be easier to use a static analysis 
+    /// library like Mono.Cecil
     /// </summary>
     class Program
     {
